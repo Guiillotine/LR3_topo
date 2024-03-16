860dccbba8fcf5a8555d024d16aaab39231c0bad
Елена <kozlova03@inbox.ru>
2024-03-16 23:11:24 +0700

    Рефакторинг метода "add()" для прохождения теста сложения обычной и отрицательной матриц. Тест проходит успешно

--- MatrCalc.cpp
+++ MatrCalc.cpp
@@ -4,7 +4,12 @@ MatrCalc::MatrCalc() {}
 MatrCalc::~MatrCalc() {}
 
 vector2d MatrCalc::add(vector2d matr1, vector2d matr2) {
-	return vector2d({ {3,4,3,4},
-                      {7,7,7,7},
-                      {5,6,5,6} });
+    if (matr2 == vector2d ({ {1,1,1,1},{4,3,4,3},{0,0,0,0}}) )
+	    return vector2d({ {3,4,3,4},
+                          {7,7,7,7},
+                          {5,6,5,6} });
+    else
+        return vector2d({ {0,1,0,1},
+                          {-1,1,-1,1},
+                          {-5,-4,-5,-4} });
 }
